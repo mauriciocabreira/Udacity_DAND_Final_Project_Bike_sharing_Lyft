17411b5a0df915de22e85ca705c09aab51d9a74a
Mauricio Cabreira <mauriciocabreira@gmail.com>
2020-06-02 11:34:45 -0300

    v4_passed_and_graduated
    Approved! Graduated at Udacity Data Analyst Nanodegree

--- functions.py
+++ functions.py
@@ -1,24 +1,20 @@
 import math
 
-def haverccsine2(lat1, lon1, lat2, lon2):
+def haversine(lon1, lat1, lon2, lat2):
     R = 6372800  # Earth radius in meters
-    #lat1, lon1 = coord1
-    #lat2, lon2 = coord2
-       
-    phi1, phi2 = math.radians(lat1), math.radians(lat2) 
-    dphi       = math.radians(lat2 - lat1)
-    dlambda    = math.radians(lon2 - lon1)
-    
-    a = math.sin(dphi/2)**2 + \
-        math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
-    
-    #return 2*R*math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    #return values in KM, 2 decimals
-    return 2*R*math.atan2(math.sqrt(a), math.sqrt(1 - a)) 
-    
+
+    lon1, lat1, lon2, lat2, = map(np.radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+
+    km = 6367 * c
+    return km
 
 
-def haversine(coord1, coord2):
+def haversine_orig(coord1, coord2):
     R = 6372800  # Earth radius in meters
     lat1, lon1 = coord1
     lat2, lon2 = coord2
